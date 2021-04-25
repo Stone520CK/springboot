97c80bdee17b2d61cf12f82553703ee78bf078ea
Stone520CK <1853048377@qq.com>
2021-04-25 23:04:49 +0800

    hello world

---
+++ src/main/java/com/test/SpringBootStart.java
@@ -0,0 +1,20 @@
+ package com.test;/**
+ * @description: test
+ *
+ * @Author Stone
+ *
+ */
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpringBootStart extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.test.controller;/**
+ * @description: test
+ *
+ * @Author Stone
+ *
+ */public class TestController {
+}
