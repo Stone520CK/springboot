66ce2028230b42530010cfb970b8de05ac23b107
Stone520CK <1853048377@qq.com>
2021-04-25 23:12:48 +0800

    hello world

--- src/main/java/com/test/SpringBootStart.java
+++ src/main/java/com/test/SpringBootStart.java
@@ -1,20 +1,23 @@
- package com.test;/**
+package com.test;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
  * @description: test
- *
  * @Author Stone
- *
  */
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class SpringBootStart extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        
+@SpringBootApplication
+//扫描控制器类
+@ComponentScan(basePackages="com.test")
+//扫描dao
+@MapperScan(basePackages="com.test.mapper")
+public class SpringBootStart {
+    public static void main(String [] args){
+        SpringApplication.run(SpringBootStart.class,args);
     }
 }
--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,7 +1,19 @@
-package com.test.controller;/**
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
  * @description: test
- *
  * @Author Stone
- *
- */public class TestController {
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/")
+    public String index(Model model, HttpServletResponse response) {
+        return "index";
+    }
 }
